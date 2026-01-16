81bb55f40205bef50024876cd6c219107d78334f
SofiaMartinez-Ramovecchi <zunildaramivecchi@gmail.com>
2026-01-16 18:22:51 -0300

    chore: (test) fix dirs and names for test sweet

---
+++ backend/src/solana/login.module.ts
@@ -0,0 +1,11 @@
+import { Module } from "@nestjs/common";
+
+import { LoginController } from "./login.controller";
+
+@Module({
+    imports: [],
+    controllers: [ LoginController ],
+    providers: []
+})
+
+export class LoginModule {}
